0e15ab3cca64297834bd097d75503f445557d446
devjsluis <devjsluis@gmail.com>
2024-07-31 22:21:55 -0600

    Initial commit with Webpack configuration, HTML, CSS, and JavaScript examples

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js", // Archivo de entrada principal
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"), // Carpeta de salida
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Para archivos CSS
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/, // Para archivos JS
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  mode: "development",
+};
